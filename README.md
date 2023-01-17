0fd5d74cf7a3adc17233d80743c1d68d4bff6dab
KristenSantos <Kristensxntos@gmail.com>
2023-01-17 15:49:50 +0000

    Completed code challenge

--- README.md
+++ README.md
@@ -26,13 +26,13 @@ const alumni = [
 {name:'Jan', job:'Square Space',language:'JavaScript', age:22},
 {name:'Uzma', job:'Thyme Care',language:'JavaScript', age:22}]
 ```
-
+Reduce
 1. Write a function named `averageAge` that takes an array of objects and returns the average of all the ages from each age property in each object rounded to the nearest whole number. 
     
     ```jsx
     averageAge(alumni) // 22
     ```
-
+Sort
 2. Write a function named `orderedAlumni` that takes an array of objects and sorts the objects by the age of the alumni from oldest to youngest. 
     
     ```jsx
@@ -55,7 +55,7 @@ const alumni = [
       { name: 'Cielo', job: 'NYT', language: 'JavaScript', age: 21 }
     ]
     ```
-
+Every
 3. Write a function named `allUseJavaScript` that takes an array of objects and returns a boolean if for every object, the language property is 'JavaScript'.
     
         ```jsx
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const alumni = [
+{name:'Jarrit', job:'TPT',language:'JavaScript', age:23}, 
+{name:'Stephanie', job:'JPMorgan',language:'JavaScript', age:24}, 
+{name:'Devonte', job:'WW',language:'JavaScript', age:23}, 
+{name:'Enmanuel', job:'Asana',language:'JavaScript', age:23},
+{name:'Shemar', job:'SquareSpace',language:'JavaScript', age:23},
+{name:'Cielo', job:'NYT',language:'JavaScript', age:22},
+{name:'Carmen', job:'Marcy Lab School',language:'JavaScript', age:21},
+{name:'Itzel', job:'Marcy Lab School',language:'JavaScript', age:22},
+{name:'Ray', job:'Square Space',language:'JavaScript', age:21},
+{name:'Jan', job:'Square Space',language:'JavaScript', age:22},
+{name:'Uzma', job:'Thyme Care',language:'JavaScript', age:22}]
 
+const averageAge = arrOfObj =>{
+    const average = arrOfObj.reduce((a,alumn) => a + alumn.age,0 )/arrOfObj.length
+    return Math.round(average)
+}
+console.log(averageAge(alumni));
+
+
+const orderedAlumni = arrOfObj =>{
+    const ordered = arrOfObj.sort((a,b) => b.age - a.age);
+    return ordered
+}
+console.log(orderedAlumni(alumni));
+
+const allUseJavaScript = arrOfObj => {
+    return arrOfObj.every(alunm => alunm.language === "JavaScript");
+}
+console.log(allUseJavaScript(alumni));
